4baa7a2f9defad3c02ffc130315328adc3257bfc
Joacim <Joacim@vetterlain.dk>
2017-04-04 12:37:35 +0200

    cleaned project

--- Frontend/src/pages/App.js
+++ Frontend/src/pages/App.js
@@ -13,7 +13,7 @@ const App = observer(class App extends Component {
       <div>
         <nav className="navbar navbar-default" >
           <div className="container-fluid">
-           
+
             <ul className="nav navbar-nav">
               <li><Link to="/home">Home</Link></li>
               <li><Link to="/documentation">Documentation</Link></li>
