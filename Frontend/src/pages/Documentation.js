b16789b5e9f457aec3fdafc4d46b8341798c29f5
Zaeem-Shafiq <zaeem1996@hotmail.com>
2017-04-06 13:31:28 +0200

    Added documentation

--- Frontend/src/pages/Documentation.js
+++ Frontend/src/pages/Documentation.js
@@ -1,7 +1,14 @@
 import React, {Component} from "react";
 
-export default class About extends Component{
-  render() {
-    return <h1>Documentation</h1>
-  }
-}
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Documentation</h1>
+                <a href="https://github.com/Zaeem-Shafiq/CA3">See GitHub</a>
+                <br/>
+                <a href="https://vetterlain.dk/CA3/api/book">See API</a>
+            </div>
+    )
+    }
+    }
